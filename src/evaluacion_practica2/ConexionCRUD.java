83f6376dd4cfb0ab3fffb0c83d6308f64d8fe340
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-05-05 09:35:41 -0600

    Constructor de la clase ConexionCRUD

--- src/evaluacion_practica2/ConexionCRUD.java
+++ src/evaluacion_practica2/ConexionCRUD.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package evaluacion_practica2;
 
-/**
- *
- * @author ITCA
- */
+import java.sql.*; //Libreria de conexiones SQL
+
+
 public class ConexionCRUD {
+    /*
+    Ruta de la base de datos del servidor 127.0.0.1, el puerto 3306 y el nombre 
+    de la base de datos bd_recurso_humano
+    */
+    private final String servidor = "jdbc:mysql://127.0.0.1:3306/bd_Estudiantes"; 
+    //Nombre del usuario (root por defecto) de la base de datos
+    private final String usuario = "root";
+    //Clave del usuario de la base de datos
+    private final String clave = "";
+    //Libreria de mysql
+    private final String driverConector  = "com.mysql.jdbc.Driver";
+    //Objeto de la clase Connection del paquete java.sql
+    private static Connection conexion;
     
+    //Constructor, que se conecta a la base de datos al ser llamado
+    public ConexionCRUD(){
+        try{
+            Class.forName(driverConector); //Levantar el driver
+            //Establecer conexión
+            conexion = DriverManager.getConnection(servidor, usuario, clave);
+            
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println("Conexión fallida! Error! : " + e.getMessage());
+        }
+    }
 }
