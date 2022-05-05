274c36037a08ffd862eb126aa67c06132b818316
nestorclash <graciehernandez022@gmail.com>
2022-05-05 10:43:22 -0600

    clase Update nuevos metodos

--- src/evaluacion_practica2/Update.java
+++ src/evaluacion_practica2/Update.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package evaluacion_practica2;
 
-/**
- *
- * @author ITCA
- */
+import java.sql.SQLException;
+
+import java.util.Scanner;
+
 public class Update {
-    
+    Update() throws SQLException{ 
+        Scanner leer= new Scanner (System.in);
+
+Persona e = new Persona();
+ConexionCRUD utilerias =new ConexionCRUD();
+
+System.out.println("<<<<< ACTUALIZAR REGISTROS >>>>>");
+
+System.out.println("Ingresar id del registro a modificar: ");
+
+e.setId_Estudiante(leer.nextInt());
+
+String tablaB="tb_Estudiantes";
+
+String campoB= "id_estudiante, carnet_estudiante, nom_estudiante, ape_estudiante, edad_estudiante";
+
+
+String condicionB="id_estudiante = "+e.getId_estudiante();
+utilerias.desplegarRegistros(tablaB, campoB, condicionB); 
+System.out.println("Nombre: ");
+
+
 }
