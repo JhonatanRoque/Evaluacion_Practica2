c2bd2c561122798f008811242628d1a27b98aa51
Criss7w7 <101343214+Criss7w7@users.noreply.github.com>
2022-05-05 11:37:30 -0600

    Clase Consultar Registro

---
+++ src/evaluacion_practica2/ConsultarRegistro.java
@@ -0,0 +1,11 @@
+package evaluacion_practica2;
+import java.sql.SQLException;
+public class ConsultarRegistro {
+    public ConsultarRegistro() throws SQLException{
+        System.out.println("------CONSULTA DE REGISTROS------");
+        mostrarResultados();
+    }
+private void mostrarResultados() throws SQLException{
+    
+}    
+}
