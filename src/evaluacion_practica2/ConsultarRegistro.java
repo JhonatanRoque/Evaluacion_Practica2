7134efc23e0947052de17ef881245621064032e5
Criss7w7 <101343214+Criss7w7@users.noreply.github.com>
2022-05-05 12:13:13 -0600

    Clase ConsultarRegistro

--- src/evaluacion_practica2/ConsultarRegistro.java
+++ src/evaluacion_practica2/ConsultarRegistro.java
@@ -10,13 +10,15 @@ private void mostrarResultados() throws SQLException{
         ConexionCRUD utilerias = new
     ConexionCRUD();
             String tabla = "tb_estudiante";
-            String composTabla = "*";
+            String camposTabla = "*";
             String condicionBuscar = "";
-            utilerias.desplegarresgistros(tabla, camposTabla, condicionBuscar);
+            utilerias.desplegarRegistros(tabla, camposTabla, condicionBuscar);
             
     
-    }catch (SQLExcetion ex){
-        System.out.println("Ha ocurrido el siguiente error: " );
+    }catch (SQLException ex){
+        System.out.println("Ha ocurrido el siguiente error: " + ex.getMessage() );
+    }finally{
+        MenuPrincipal.desplegarMenu();
     }
 }    
 }
