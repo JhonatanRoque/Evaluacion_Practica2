c1927474ec6e53f24d36e46af685b71904129087
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-05-05 10:07:01 -0600

    Clase MenuPrincipal

--- src/evaluacion_practica2/MenuPrincipal.java
+++ src/evaluacion_practica2/MenuPrincipal.java
@@ -1,21 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package evaluacion_practica2;
 
-/**
- *
- * @author ITCA
- */
+import java.sql.SQLException; //Libreria para conexiones con base de datos
+import java.util.*;
+
 public class MenuPrincipal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        desplegarMenu();
+    }
+    
+    //metodo de desplegar menu
+    public static void desplegarMenu() {
+        Scanner opcionSeleccionada = new Scanner(System.in);
+        String opcionMenu;
+        //Menu a desplegar
+        System.out.println("*******************************************");
+        System.out.println("|         CRUD  DE JAVA EN CONSOLA        |");
+        System.out.println("| Opciones:                               |");
+        System.out.println("|          1. Crear resgistro             |");
+        System.out.println("|          2. Consultar registros         |");
+        System.out.println("|          3. Actualizar registros        |");
+        System.out.println("|          4. Eliminar registros          |");
+        System.out.println("|          5. Salir                       |");
+        System.out.println("*******************************************");
+        System.out.println("Seleccionar opción: ");
+        opcionMenu = opcionSeleccionada.next();
+        try{
+        //Despliegue de menu basado en la opción seleccionada
+        switch(opcionMenu){
+            case "1":
+                //Create create = new Create();
+                break;
+            case "2":
+                //Read read = new Read();
+                break;
+            case "3":
+                //Update update = new Update();
+                break;
+            case "4":
+                //Delete delete = new Delete();
+                break;
+            case "5":
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Selección invalida!");
+                break;
+        }
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
     }
     
 }
