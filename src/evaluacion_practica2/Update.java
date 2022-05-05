d717e14a6482f20148bdd4bba67cd7f7d2412477
nestorclash <graciehernandez022@gmail.com>
2022-05-05 10:44:51 -0600

    clase Update completada

--- src/evaluacion_practica2/Update.java
+++ src/evaluacion_practica2/Update.java
@@ -26,5 +26,15 @@ String condicionB="id_estudiante = "+e.getId_estudiante();
 utilerias.desplegarRegistros(tablaB, campoB, condicionB); 
 System.out.println("Nombre: ");
 
+e.setNom_estudiante(leer.next());
+System.out.println("Apellido: ");
+e.setApe_estudiante(leer.next());
+System.out.println("Carnet: ");
+e.setCarnet_estudiante(leer.nextLine()); 
 
+String camposValoresnuevos="carnet_estudiante='"+e.getCarnet_estudiante()+"', nom_estudiante= '"+e.getNom_estudiante()+"', ape_estudiante='"+e.getApe_estudiante()+"', edad_estudiante='"+e.getEdad_estudiante();
+
+utilerias.actualizarEliminarRegistro(tablaB, camposValoresnuevos, condicionB); 
+System.out.println("Modificado correctamente!"); MenuPrincipal.desplegarMenu();
+} 
 }
